afb4dc19f96bddaed5c2d2ca030919cc894484d1
Eduard Marisov <edmariso@microsoft.com>
2021-01-25 21:48:14 +0100

    String and dictionary benchmarks ;

---
+++ Benchmarks/DictionaryBenchmarks.cs
@@ -0,0 +1,75 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Order;
+using System;
+using System.Collections.Generic;
+
+namespace Benchmarks
+{
+    [SimpleJob(RunStrategy.Throughput)]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [MinColumn, MaxColumn, Q3Column]
+    public class DictionaryBenchmarks
+    {
+        private readonly Dictionary<string, string> invariantComparerDict;
+        private readonly Dictionary<string, string> defaultComparerDict;
+
+        public DictionaryBenchmarks()
+        {
+            string[] keys = new[] { "key1", "key2", "key" };
+            invariantComparerDict = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
+            defaultComparerDict = new Dictionary<string, string>();
+            foreach(var key in keys)
+            {
+                invariantComparerDict.Add(key, "TRUE");
+                defaultComparerDict.Add(key, "TRUE");
+            }
+        }
+
+        [Benchmark]
+        public bool InvariantDictTryGetValue()
+        {
+            if (invariantComparerDict.TryGetValue("KEY", out string value) && "true".Equals(value, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        [Benchmark]
+        public bool DefaultDictContainsGetValue()
+        {
+            if (defaultComparerDict.ContainsKey("KEY".ToLowerInvariant()) && defaultComparerDict["KEY".ToLowerInvariant()].ToLowerInvariant() == "true")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        [Benchmark]
+        public bool DefaultDictTryGetValueCompareWithEqualsInvariantCulture()
+        {
+            string key = "KEY".ToLowerInvariant();
+            if (defaultComparerDict.TryGetValue(key, out string value) && "true".Equals(value, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        [Benchmark]
+        public bool DefaultDictTryGetValueCompareWithEqualityOperator()
+        {
+            string key = "KEY".ToLowerInvariant();
+            if (defaultComparerDict.TryGetValue(key, out string value) && value.ToLowerInvariant() == "true")
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Benchmarks/StringBenchmarks.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Order;
+using System;
+
+namespace Benchmarks
+{
+    [SimpleJob(RunStrategy.Throughput)]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    [MinColumn, MaxColumn, Q3Column]
+    public class StringBenchmarks
+    {
+        [Benchmark]
+        [Arguments("true")]
+        [Arguments("truE")]
+        [Arguments("TRUE")]
+        [Arguments("false")]
+        public bool InvariantCultureEquals(string value)
+        {
+            return "true".Equals(value, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        [Benchmark]
+        [Arguments("true")]
+        [Arguments("truE")]
+        [Arguments("TRUE")]
+        [Arguments("false")]
+        public bool OneToLowerInvariantOperatorEquals(string value)
+        {
+            return "true" == value.ToLowerInvariant();
+        }
+
+        [Benchmark]
+        [Arguments("true")]
+        [Arguments("truE")]
+        [Arguments("TRUE")]
+        [Arguments("false")]
+        public bool BothToLowerInvariantOperatorEquals(string value)
+        {
+            return "true".ToLowerInvariant() == value.ToLowerInvariant();
+        }
+    }
+}
